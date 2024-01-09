8f785e48428bceb9f5a9de19e653791baa2fc392
Mariama1001 <154673023+Mariama1001@users.noreply.github.com>
2024-01-09 17:41:39 +0100

    Update ibgp.py

--- ibgp.py
+++ ibgp.py
@@ -1,6 +1,6 @@
 import ipaddress
 
-def ibgp_config(output_file,interface, as_number,neighbor_router_ip):
+def ibgp_config(output_file,interface, as_number,neighbor_router_ip,router_id):
     output_file.write(f"R{router_id}# configure terminal\n")
     output_file.write(f"R{router_id}# interface {interface}\n")
     output_file.write(f"R{router_id}# router bgp {as_number}\n")
