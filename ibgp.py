61c7707caf188a509aca73a95c5bea85b5b2ff93
unicorn <118615352+ZixinP@users.noreply.github.com>
2024-01-11 19:39:31 +0100

    Delete ibgp.py

--- ibgp.py
+++
@@ -1,8 +0,0 @@
-import ipaddress
-
-def ibgp_config(output_file,interface, as_number,neighbor_router_ip,router_id):
-    output_file.write(f"R{router_id}# configure terminal\n")
-    output_file.write(f"R{router_id}# interface {interface}\n")
-    output_file.write(f"R{router_id}# router bgp {as_number}\n")
-    output_file.write(f"R{router_id}# neighbor {neighbor_router_ip} remote-as {as_number}\n")
-    output_file.write(f"R{router_id}# end \n")
