97a79c442b6250a7b764910b3b151cf088ca1fa8
NuageZHU <zhukuangyun@gmail.com>
2024-01-31 15:49:27 +0100

    ，。

--- exemple_readwritejson/readJson.py
+++
@@ -1,5 +0,0 @@
-import json
-with open("books.json")as json_file:
-    data=json.load(json_file)
-    for book in data["books"]:
-        print(book["title"],"by",book["author"],"published in",book["published"])
